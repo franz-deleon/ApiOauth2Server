3c14e104075bc226ba4fa79cbca7f54292eeb62e
Franz De Leon <fleo@osidev-imac2.loctest.gov>
2013-11-15 16:07:22 -0500

    add entities

--- config/module.config.php
+++ config/module.config.php
@@ -5,7 +5,23 @@ return array(
 
         ),
         'invokables' => array(
+            /** Storages **/
             'ApiOauth2Server/Storage/UserCredentials' => 'ApiOauth2Server\Storage\UserCredentials',
+            'ApiOauth2Server/Storage/AccessToken'     => 'ApiOauth2Server\Storage\AccessToken',
+
+            /** Entities **/
+            'ApiOauth2Server/Model/Entity/AccessToken'  => 'ApiOauth2Server\Model\Entity\OAuthAccessToken',
+            'ApiOauth2Server/Model/Entity/Client'       => 'ApiOauth2Server\Model\Entity\OAuthClient',
+            'ApiOauth2Server/Model/Entity/RefreshToken' => 'ApiOauth2Server\Model\Entity\OAuthRefreshToken',
+            'ApiOauth2Server/Model/Entity/Scope'        => 'ApiOauth2Server\Model\Entity\OAuthScope',
+            'ApiOauth2Server/Model/Entity/User'         => 'ApiOauth2Server\Model\Entity\OAuthUser',
+        ),
+        'shared' => array(
+            'ApiOauth2Server/Model/Entity/AccessToken'  => false,
+            'ApiOauth2Server/Model/Entity/Client'       => false,
+            'ApiOauth2Server/Model/Entity/RefreshToken' => false,
+            'ApiOauth2Server/Model/Entity/Scope'        => false,
+            'ApiOauth2Server/Model/Entity/User'         => false,
         ),
     ),
     'doctrine' => include_once __DIR__ . '/doctrine.config.php',
