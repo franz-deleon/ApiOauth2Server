f534bb463ad5f2739295d88b7f4e7656059f7e71
Franz De Leon <fleo@osidev-imac2.loctest.gov>
2013-11-29 16:23:32 -0500

    added asset storages

---
+++ tests/ApiOauth2ServerTests/Assets/ClientCredentialsStorage.php
@@ -0,0 +1,55 @@
+<?php
+namespace ApiOauth2ServerTests\Assets;
+
+use OAuth2\Storage\ClientCredentialsInterface;
+
+class ClientCredentialsStorage implements ClientCredentialsInterface
+{
+    /**
+     * Make sure that the client credentials is valid.
+     *
+     * @param $client_id
+     * Client identifier to be check with.
+     * @param $client_secret
+     * (optional) If a secret is required, check that they've given the right one.
+     *
+     * @return
+     * TRUE if the client credentials are valid, and MUST return FALSE if it isn't.
+     * @endcode
+     *
+     * @see http://tools.ietf.org/html/rfc6749#section-3.1
+     *
+     * @ingroup oauth2_section_3
+     */
+    public function checkClientCredentials($client_id, $client_secret = null)
+    {
+
+    }
+
+    public function getClientDetails($client_id)
+    {
+
+    }
+
+    /**
+     * Check restricted grant types of corresponding client identifier.
+     *
+     * If you want to restrict clients to certain grant types, override this
+     * function.
+     *
+     * @param $client_id
+     * Client identifier to be check with.
+     * @param $grant_type
+     * Grant type to be check with
+     *
+     * @return
+     * TRUE if the grant type is supported by this client identifier, and
+     * FALSE if it isn't.
+     *
+     * @ingroup oauth2_section_4
+    */
+    public function checkRestrictedGrantType($client_id, $grant_type)
+    {
+
+    }
+}
